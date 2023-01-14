21efb42985c8848ed47439108e4a543871afbe7e
lrucker1 <lrucker@mac.com>
2023-01-13 18:23:39 -0800

    Update jr_visca.c
    Major changes to make it easier to add more commands: 
    Macros to handle the common forms of "definitions" entries and generic responses, so adding a new command is only two lines of code plus the enums
    A new numbering convention for the enums based on the visca command ID, which makes it easier to debug and rearrange code
    Enough new  visca commands to test all the values that PTZOptics includes in presets in the newest firmware.

--- jr_visca.c
+++ jr_visca.c
@@ -131,6 +131,23 @@ void _jr_viscaWrite16ToBuffer(int16_t value, uint8_t *buffer) {
     buffer[3] |= value & 0xf;
 }
 
+/**
+ * `buffer` looks like 0x01 0x02
+ * Returned result will look like 0x12
+ * This is a common way for VISCA to bit pack things.
+ */
+int16_t _jr_viscaRead8FromBuffer(uint8_t *buffer) {
+    int16_t result = 0;
+    result += (buffer[0] & 0xf) * 0x10;
+    result += (buffer[1] & 0xf);
+    return result;
+}
+
+void _jr_viscaWrite8ToBuffer(int16_t value, uint8_t *buffer) {
+    buffer[0] |= (value >> 4) & 0xf;
+    buffer[1] |= value & 0xf;
+}
+
 void jr_visca_handlePanTiltPositionInqResponseParameters(jr_viscaFrame* frame, union jr_viscaMessageParameters *messageParameters, bool isDecodingFrame) {
     if (isDecodingFrame) {
         messageParameters->panTiltPositionInqResponseParameters.panPosition = _jr_viscaRead16FromBuffer(frame->data + 1);
@@ -160,19 +177,22 @@ void jr_visca_handleAbsolutePanTiltPositionParameters(jr_viscaFrame* frame, unio
     }
 }
 
-void jr_visca_handleZoomPositionInqResponseParameters(jr_viscaFrame* frame, union jr_viscaMessageParameters *messageParameters, bool isDecodingFrame) {
-    if (isDecodingFrame) {
-        messageParameters->zoomPositionParameters.zoomPosition = _jr_viscaRead16FromBuffer(frame->data + 1);
-    } else {
-        _jr_viscaWrite16ToBuffer(messageParameters->zoomPositionParameters.zoomPosition, frame->data + 1);
-    }
-}
-
 void jr_visca_handleAckCompletionParameters(jr_viscaFrame* frame, union jr_viscaMessageParameters *messageParameters, bool isDecodingFrame) {
     if (isDecodingFrame) {
         messageParameters->ackCompletionParameters.socketNumber = frame->data[0] & 0xf;
     } else {
         frame->data[0] += messageParameters->ackCompletionParameters.socketNumber;
+    }
+}
+
+
+void jr_visca_handleErrorReplyParameters(jr_viscaFrame* frame, union jr_viscaMessageParameters *messageParameters, bool isDecodingFrame) {
+    if (isDecodingFrame) {
+        messageParameters->errorReplyParameters.socketNumber = frame->data[0] & 0xf;
+        messageParameters->errorReplyParameters.errorType = frame->data[1];
+    } else {
+        frame->data[0] += messageParameters->errorReplyParameters.socketNumber;
+        frame->data[1] += messageParameters->errorReplyParameters.errorType;
     }
 }
 
@@ -212,11 +232,19 @@ void jr_visca_handleMemoryParameters(jr_viscaFrame* frame, union jr_viscaMessage
     }
 }
 
-void jr_visca_handleZoomDirectParameters(jr_viscaFrame* frame, union jr_viscaMessageParameters *messageParameters, bool isDecodingFrame) {
+void jr_visca_handlePQRSCommandParameters(jr_viscaFrame* frame, union jr_viscaMessageParameters *messageParameters, bool isDecodingFrame) {
     if (isDecodingFrame) {
-        messageParameters->zoomPositionParameters.zoomPosition = _jr_viscaRead16FromBuffer(frame->data + 3);
+        messageParameters->int16Parameters.int16Value = _jr_viscaRead16FromBuffer(frame->data + 3);
     } else {
-        _jr_viscaWrite16ToBuffer(messageParameters->zoomPositionParameters.zoomPosition, frame->data + 3);
+        _jr_viscaWrite16ToBuffer(messageParameters->int16Parameters.int16Value, frame->data + 3);
+    }
+}
+
+void jr_visca_handlePQCommandParameters(jr_viscaFrame* frame, union jr_viscaMessageParameters *messageParameters, bool isDecodingFrame) {
+    if (isDecodingFrame) {
+        messageParameters->int16Parameters.int16Value = _jr_viscaRead8FromBuffer(frame->data + 3);
+    } else {
+        _jr_viscaWrite8ToBuffer(messageParameters->int16Parameters.int16Value, frame->data + 3);
     }
 }
 
@@ -234,7 +262,141 @@ void jr_visca_handlePanTiltDriveParameters(jr_viscaFrame* frame, union jr_viscaM
     }
 }
 
+void jr_visca_handleOneByteInqResponseParameters(jr_viscaFrame* frame, union jr_viscaMessageParameters *messageParameters, bool isDecodingFrame) {
+    if (isDecodingFrame) {
+        messageParameters->oneByteParameters.byteValue = frame->data[1] & 0xff;
+    } else {
+        frame->data[1] = messageParameters->oneByteParameters.byteValue;
+    }
+}
+
+void jr_visca_handlePInqResponseParameters(jr_viscaFrame* frame, union jr_viscaMessageParameters *messageParameters, bool isDecodingFrame) {
+    if (isDecodingFrame) {
+        messageParameters->oneByteParameters.byteValue = frame->data[1] & 0x0f;
+    } else {
+        frame->data[1] = messageParameters->oneByteParameters.byteValue;
+    }
+}
+
+void jr_visca_handlePQRSInqResponseParameters(jr_viscaFrame* frame, union jr_viscaMessageParameters *messageParameters, bool isDecodingFrame) {
+    if (isDecodingFrame) {
+        messageParameters->int16Parameters.int16Value = _jr_viscaRead16FromBuffer(frame->data + 1);
+    } else {
+        _jr_viscaWrite16ToBuffer(messageParameters->int16Parameters.int16Value, frame->data + 1);
+    }
+}
+void jr_visca_handlePQInqResponseParameters(jr_viscaFrame* frame, union jr_viscaMessageParameters *messageParameters, bool isDecodingFrame) {
+    if (isDecodingFrame) {
+        messageParameters->int16Parameters.int16Value = _jr_viscaRead8FromBuffer(frame->data + 1);
+    } else {
+        _jr_viscaWrite8ToBuffer(messageParameters->int16Parameters.int16Value, frame->data + 1);
+    }
+}
+
+void jr_visca_handleOneByteCommandParameters(jr_viscaFrame* frame, union jr_viscaMessageParameters *messageParameters, bool isDecodingFrame) {
+    if (isDecodingFrame) {
+        messageParameters->oneByteParameters.byteValue = frame->data[3] & 0xff;
+    } else {
+        frame->data[3] = messageParameters->oneByteParameters.byteValue;
+    }
+}
+
+void jr_visca_handlePCommandParameters(jr_viscaFrame* frame, union jr_viscaMessageParameters *messageParameters, bool isDecodingFrame) {
+    if (isDecodingFrame) {
+        messageParameters->oneByteParameters.byteValue = frame->data[3] & 0x0f;
+    } else {
+        frame->data[3] = messageParameters->oneByteParameters.byteValue;
+    }
+}
+
+#define MESSAGE_INQ(_cmd, _enum)    \
+{                                   \
+    {0x09, 0x04, (_cmd)},           \
+    {0xff, 0xff, 0xff},             \
+    3,                              \
+    (_enum),                        \
+    NULL                            \
+}
+
+#define MESSAGE_ONE_BYTE_VALUE_SET(_cmd, _enum) \
+{                                   \
+    {0x01, 0x04, (_cmd), 0x00},     \
+    {0xff, 0xff, 0xff, 0x00},       \
+    4,                              \
+    (_enum),                        \
+    &jr_visca_handleOneByteCommandParameters \
+}
+
+#define MESSAGE_P_VALUE_SET(_cmd, _enum) \
+{                                   \
+    {0x01, 0x04, (_cmd), 0x00},     \
+    {0xff, 0xff, 0xff, 0xf0},       \
+    4,                              \
+    (_enum),                        \
+    &jr_visca_handlePCommandParameters \
+}
+
+#define MESSAGE_PQ_VALUE_SET(_cmd, _enum)   \
+{                                           \
+    {0x01, 0x04, (_cmd), 0x00, 0x00},       \
+    {0xff, 0xff, 0xff, 0xf0, 0xf0},         \
+    5,                                      \
+    (_enum),                                \
+    &jr_visca_handlePQCommandParameters     \
+}
+
+#define MESSAGE_PQRS_VALUE_SET(_cmd, _enum)   \
+{                                               \
+    {0x01, 0x04, (_cmd), 0x00, 0x00, 0x00, 0x00},\
+    {0xff, 0xff, 0xff, 0xf0, 0xf0, 0xf0, 0xf0}, \
+    7,                                          \
+    (_enum),                                    \
+    &jr_visca_handlePQRSCommandParameters       \
+}
+
+#define MESSAGE_SUBCOMMAND_SET(_cmd, _sub, _enum)   \
+{                                   \
+    {0x01, 0x04, (_cmd), (_sub)},   \
+    {0xff, 0xff, 0xff, 0xff},       \
+    4,                              \
+    (_enum),                        \
+    NULL                            \
+}
+
+#pragma mark definitions
 jr_viscaMessageDefinition definitions[] = {
+    // Generic 1-byte response: [90 50 xx FF] and ok for [90 50 0p FF]
+    {
+        {0x50, 0x00},
+        {0xff, 0x00},
+        2,
+        JR_VISCA_MESSAGE_ONE_BYTE_RESPONSE,
+        &jr_visca_handleOneByteInqResponseParameters
+    },
+    // Generic 1-byte response: [90 50 0p FF] when you need the mask
+    {
+        {0x50, 0x00},
+        {0xff, 0xf0},
+        2,
+        JR_VISCA_MESSAGE_P_RESPONSE,
+        &jr_visca_handlePInqResponseParameters
+    },
+    // Generic response: 90 50 0p 0q 0r 0s FF
+    {
+        {0x50, 0x00, 0x00, 0x00, 0x00},
+        {0xff, 0xf0, 0xf0, 0xf0, 0xf0},
+        5,
+        JR_VISCA_MESSAGE_PQRS_INQ_RESPONSE,
+        &jr_visca_handlePQRSInqResponseParameters
+    },
+    // Generic response: 90 50 0p 0q FF
+    {
+        {0x50, 0x00, 0x00},
+        {0xff, 0xf0, 0xf0},
+        3,
+        JR_VISCA_MESSAGE_PQ_INQ_RESPONSE,
+        &jr_visca_handlePQInqResponseParameters
+    },
     {
         {0x09, 0x06, 0x12}, //signature
         {0xff, 0xff, 0xff}, //signatureMask
@@ -253,34 +415,6 @@ jr_viscaMessageDefinition definitions[] = {
         &jr_visca_handlePanTiltPositionInqResponseParameters
     },
     {
-        {0x09, 0x04, 0x47},
-        {0xff, 0xff, 0xff},
-        3,
-        JR_VISCA_MESSAGE_ZOOM_POSITION_INQ,
-        NULL
-    },
-    {
-        {0x50, 0x00, 0x00, 0x00, 0x00},
-        {0xff, 0xf0, 0xf0, 0xf0, 0xf0},
-        5,
-        JR_VISCA_MESSAGE_ZOOM_POSITION_INQ_RESPONSE,
-        &jr_visca_handleZoomPositionInqResponseParameters
-    },
-    {
-        {0x01, 0x04, 0x38, 0x02},
-        {0xff, 0xff, 0xff, 0xff},
-        4,
-        JR_VISCA_MESSAGE_FOCUS_AUTOMATIC,
-        NULL
-    },
-    {
-        {0x01, 0x04, 0x38, 0x03},
-        {0xff, 0xff, 0xff, 0xff},
-        4,
-        JR_VISCA_MESSAGE_FOCUS_MANUAL,
-        NULL
-    },
-    {
         {0x40},
         {0xf0},
         1,
@@ -294,27 +428,9 @@ jr_viscaMessageDefinition definitions[] = {
         JR_VISCA_MESSAGE_COMPLETION,
         &jr_visca_handleAckCompletionParameters
     },
-    {
-        {0x01, 0x04, 0x07, 0x00},
-        {0xff, 0xff, 0xff, 0xff},
-        4,
-        JR_VISCA_MESSAGE_ZOOM_STOP,
-        NULL
-    },
-    {
-        {0x01, 0x04, 0x07, 0x02},
-        {0xff, 0xff, 0xff, 0xff},
-        4,
-        JR_VISCA_MESSAGE_ZOOM_TELE_STANDARD,
-        NULL
-    },
-    {
-        {0x01, 0x04, 0x07, 0x03},
-        {0xff, 0xff, 0xff, 0xff},
-        4,
-        JR_VISCA_MESSAGE_ZOOM_WIDE_STANDARD,
-        NULL
-    },
+    MESSAGE_SUBCOMMAND_SET(0x07, 0x00, JR_VISCA_MESSAGE_ZOOM_STOP),
+    MESSAGE_SUBCOMMAND_SET(0x07, 0x02, JR_VISCA_MESSAGE_ZOOM_TELE_STANDARD),
+    MESSAGE_SUBCOMMAND_SET(0x07, 0x03, JR_VISCA_MESSAGE_ZOOM_WIDE_STANDARD),
     {
         {0x01, 0x04, 0x07, 0x20},
         {0xff, 0xff, 0xff, 0xf0},
@@ -329,13 +445,7 @@ jr_viscaMessageDefinition definitions[] = {
         JR_VISCA_MESSAGE_ZOOM_WIDE_VARIABLE,
         &jr_visca_handleZoomVariableParameters
     },
-    {
-        {0x01, 0x04, 0x47, 0x00, 0x00, 0x00, 0x00},
-        {0xff, 0xff, 0xff, 0xf0, 0xf0, 0xf0, 0xf0},
-        7,
-        JR_VISCA_MESSAGE_ZOOM_DIRECT,
-        &jr_visca_handleZoomDirectParameters
-    },
+    // Pan_TiltDrive
     {
         {0x01, 0x06, 0x01, 0x00, 0x00, 0x00, 0x00},
         {0xff, 0xff, 0xff, 0xe0, 0xe0, 0xf0, 0xf0},
@@ -400,12 +510,84 @@ jr_viscaMessageDefinition definitions[] = {
         NULL
     },
     {
-        {0x60, 0x04},
-        {0xf0, 0xff},
+        {0x60, 0x00},
+        {0xf0, 0x00},
         2,
-        JR_VISCA_MESSAGE_CANCEL_REPLY,
-        &jr_visca_handleAckCompletionParameters
+        JR_VISCA_MESSAGE_ERROR_REPLY,
+        &jr_visca_handleErrorReplyParameters
     },
+    // CAM_Bright 81 01 04 0D 00 00 0p 0q FF
+    MESSAGE_PQRS_VALUE_SET(0x0D, JR_VISCA_MESSAGE_BRIGHT_DIRECT),
+    MESSAGE_INQ(0x4D, JR_VISCA_MESSAGE_BRIGHT_POS_INQ),
+    // CAM_ColorTemp Direct 81 01 04 20 0p 0q FF
+    MESSAGE_PQ_VALUE_SET(0x20, JR_VISCA_MESSAGE_COLOR_TEMP_DIRECT),
+    MESSAGE_INQ(0x20, JR_VISCA_MESSAGE_COLOR_TEMP_INQ),
+    // CAM_Flicker CAM_Flicker 81 01 04 23 xx FF
+    // p: Flicker Settings - (0: Off, 1: 50Hz, 2: 60Hz)
+    MESSAGE_ONE_BYTE_VALUE_SET(0x23, JR_VISCA_MESSAGE_FLICKER_MODE),
+    MESSAGE_INQ(0x55, JR_VISCA_MESSAGE_FLICKER_MODE_INQ),
+    // CAM_Gain Gain Limit 81 01 04 2C 0p FF
+    MESSAGE_P_VALUE_SET(0x2C, JR_VISCA_MESSAGE_GAIN_LIMIT),
+    MESSAGE_INQ(0x2C, JR_VISCA_MESSAGE_GAIN_LIMIT_INQ),
+    // CAM_WB  81 01 04 35 xx FF
+    // xx is one of [01 02 03 05 20]
+    MESSAGE_ONE_BYTE_VALUE_SET(0x35, JR_VISCA_MESSAGE_WB_MODE),
+    MESSAGE_INQ(0x35, JR_VISCA_MESSAGE_WB_MODE_INQ),
+    // CAM_Focus
+    // Reply:AF 90 50 02 FF
+    // Reply:M  90 50 03 FF
+    MESSAGE_SUBCOMMAND_SET(0x38, 0x02, JR_VISCA_MESSAGE_FOCUS_AUTOMATIC),
+    MESSAGE_SUBCOMMAND_SET(0x38, 0x03, JR_VISCA_MESSAGE_FOCUS_MANUAL),
+    MESSAGE_INQ(0x38, JR_VISCA_MESSAGE_FOCUS_AF_MODE_INQ),
+    // CAM_AE 81 01 04 39 xx FF
+    // xx is [00 03 0A 0B 0D]
+    MESSAGE_ONE_BYTE_VALUE_SET(0x39, JR_VISCA_MESSAGE_AE_MODE),
+    MESSAGE_INQ(0x39, JR_VISCA_MESSAGE_AE_MODE_INQ),
+    // CAM_Aperture(sharpness) 81 01 04 42 00 00 0p 0q FF
+    MESSAGE_PQRS_VALUE_SET(0x42, JR_VISCA_MESSAGE_APERTURE_VALUE),
+    MESSAGE_INQ(0x42, JR_VISCA_MESSAGE_APERTURE_VALUE_INQ),
+    // CAM_RGain 81 01 04 43 00 00 0p 0q FF
+    MESSAGE_PQRS_VALUE_SET(0x43, JR_VISCA_MESSAGE_RGAIN_VALUE),
+    MESSAGE_INQ(0x43, JR_VISCA_MESSAGE_RGAIN_VALUE_INQ),
+    // CAM_BGain 81 01 04 44 00 00 0p 0q FF
+    MESSAGE_PQRS_VALUE_SET(0x44, JR_VISCA_MESSAGE_BGAIN_VALUE),
+    MESSAGE_INQ(0x44, JR_VISCA_MESSAGE_BGAIN_VALUE_INQ),
+    // CAM_Zoom Direct 81 01 04 47 p q r s FF
+    MESSAGE_PQRS_VALUE_SET(0x47, JR_VISCA_MESSAGE_ZOOM_DIRECT),
+    MESSAGE_INQ(0x47, JR_VISCA_MESSAGE_ZOOM_POSITION_INQ),
+    // CAM_Focus Direct 81 01 04 48 0p 0q 0r 0s FF
+    MESSAGE_PQRS_VALUE_SET(0x48, JR_VISCA_MESSAGE_FOCUS_VALUE),
+    MESSAGE_INQ(0x48, JR_VISCA_MESSAGE_FOCUS_VALUE_INQ),
+    // CAM_ColorGain Direct 81 01 04 49 00 00 00 0p FF
+    // p: Color Gain setting 0h (60%) to Eh (200%)
+    MESSAGE_PQRS_VALUE_SET(0x49, JR_VISCA_MESSAGE_COLOR_GAIN_DIRECT),
+    MESSAGE_INQ(0x49, JR_VISCA_MESSAGE_COLOR_GAIN_INQ),
+    // CAM_ColorHue Direct 81 01 04 4F 00 00 00 0p FF
+    // p: Color Hue setting 0h (− 14 dgrees) to Eh ( +14 degrees)
+    MESSAGE_PQRS_VALUE_SET(0x4F, JR_VISCA_MESSAGE_COLOR_HUE_DIRECT),
+    MESSAGE_INQ(0x4F, JR_VISCA_MESSAGE_COLOR_HUE_INQ),
+    // CAM_LR_Reverse (flipH)  81 01 04 61 xx FF
+    // xx is off/on [02 03]
+    MESSAGE_ONE_BYTE_VALUE_SET(0x61, JR_VISCA_MESSAGE_LR_REVERSE),
+    MESSAGE_INQ(0x61, JR_VISCA_MESSAGE_LR_REVERSE_INQ),
+    // CAM_PictureEffect  81 01 04 63 xx FF
+    // xx is (00:Off, 04:B&W), or others depending on camera
+    MESSAGE_ONE_BYTE_VALUE_SET(0x63, JR_VISCA_MESSAGE_PICTURE_EFFECT),
+    MESSAGE_INQ(0x63, JR_VISCA_MESSAGE_PICTURE_EFFECT_INQ),
+    // CAM_PictureFlip (flipV) 81 01 04 66 02 FF
+    // xx is off/on [02 03]
+    MESSAGE_ONE_BYTE_VALUE_SET(0x66, JR_VISCA_MESSAGE_PICTURE_FLIP),
+    MESSAGE_INQ(0x66, JR_VISCA_MESSAGE_PICTURE_FLIP_INQ),
+    // CAM_Brightness Direct 81 01 04 A1 00 00 0p 0q FF
+    MESSAGE_PQRS_VALUE_SET(0xA1, JR_VISCA_MESSAGE_BRIGHTNESS),
+    MESSAGE_INQ(0xA1, JR_VISCA_MESSAGE_BRIGHTNESS_INQ),
+    // CAM_Contrast Direct 81 01 04 A2 00 00 0p 0q FF
+    MESSAGE_PQRS_VALUE_SET(0xA1, JR_VISCA_MESSAGE_CONTRAST),
+    MESSAGE_INQ(0xA1, JR_VISCA_MESSAGE_CONTRAST_INQ),
+    // CAM_AWBSensitivity 81 01 04 A9 xx FF
+    // xx is 00=high 01=normal 02=low
+    MESSAGE_ONE_BYTE_VALUE_SET(0xA9, JR_VISCA_MESSAGE_AWB_SENS),
+    MESSAGE_INQ(0xA9, JR_VISCA_MESSAGE_AWB_SENS_INQ),
     { {}, {}, 0, 0, NULL} // Final definition must have `signatureLength` == 0.
 };
 
